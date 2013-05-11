a6aca8dd495482a33eb466ea528c039882fe9100
Matt Ray <matthewhray@gmail.com>
2013-05-11 10:53:13 -0500

    working dashboard role

--- roles/os-dashboard.rb
+++ roles/os-dashboard.rb
@@ -2,5 +2,15 @@ name "os-dashboard"
 description "Horizon server"
 run_list(
   "role[os-base]",
-  "recipe[os-dashboard::server]"
+  "recipe[openstack-dashboard::db]",
+  "recipe[openstack-dashboard::server]"
+  )
+
+
+override_attributes(
+  "openstack" => {
+    "role" => {
+      "dashboard" => "os-dashboard"
+    }
+  }
   )
